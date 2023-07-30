88f1aaaf1983f5b76cc184989022e8fd629416f5
oyaki <kohei089@keio.jp>
2023-07-31 02:30:54 +0900

    asm編集

--- grade.asm
+++ grade.asm
@@ -2,26 +2,26 @@
 loop: addi x1,x1,-4
 	lw x3,x1,0
 	slti x4,x3,85
-	bne x4,x0,btest
 	addi x5,x0,5
+	bne x4,x0,btest
 	sw x5,x1,400
 	beq x1,x0,exit
 	beq x0,x0,loop
 btest: slti x4,x3,60
-	bne x4,x0,ctest
 	addi x5,x0,4
+	bne x4,x0,ctest
 	sw x5,x1,400
 	beq x1,x0,exit
 	beq x0,x0,loop
 ctest: slti x4,x3,40
-	bne x4,x0,dtest
 	addi x5,x0,3
+	bne x4,x0,dtest
 	sw x5,x1,400
 	beq x1,x0,exit
 	beq x0,x0,loop
 dtest: slti x4,x3,10
-	bne x4,x0,etest
 	addi x5,x0,2
+	bne x4,x0,etest
 	sw x5,x1,400
 	beq x1,x0,exit
 	beq x0,x0,loop
@@ -29,4 +29,5 @@ etest: addi x5,x0,1
 	sw x5,x1,400
 	beq x1,x0,exit
 	beq x0,x0,loop
-exit: ecall x0,x0,x0
+exit: add x0, x0, x0
+	ecall x0,x0,x0
